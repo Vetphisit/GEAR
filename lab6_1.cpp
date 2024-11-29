4c1e12560fe22262ba6bd0356e549aa3faef7b66
Vetphisit <vetpisit07@gmail.com>
2024-11-29 16:16:34 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,24 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int n=1;
+    int Even=0;
+    int Odd=0;
+    
+    while(n!=0){
+        cout << "Enter an integer: ";
+        cin>>n;
+        if (n>0 && n%2==0){
+            Even = Even+1;
+        }
+        else if (n>0 && n%2==1){
+            Odd = Odd+1;
+        }
+        
+    }
+    cout << "#Even numbers = "<<Even<<endl;
+    cout << "#Odd numbers = "<<Odd;
     return 0;
+
+
 }
