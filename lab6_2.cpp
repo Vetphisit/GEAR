e089f1001568628e167bc5f721c4690928dae83d
Vetphisit <vetpisit07@gmail.com>
2024-12-03 13:23:59 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+
+double deg2rad(double deegree){
+    return deegree*M_PI/180;
+}
+double radian(double radian){
+    return radain*180/M_PI;
+}
+double findXComponent()
+
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
